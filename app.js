af1215d01887abc687de2daec1bc9e2f0b882146
Yevgeny <unstoo@gmail.com>
2017-02-23 04:45:29 +0300

    update

---
+++ app.js
@@ -0,0 +1,28 @@
+// var log = console.log;
+
+// function sum(a,b, cb) {
+ 
+//   setTimeout(() => {
+//     log("in setTimeout: ", a, b);
+//     cb( a + b )
+//   });
+
+// }
+
+
+// function sumArr(arr, cb) {
+
+//   while(arr.length) {
+
+//     log(arr.shift());
+
+//   }
+
+// }
+
+// //sumArr([1,2,5,5,3]);
+
+
+// sum(3, 7, (total   /*a + b*/) => {
+//   log(total /*a + b*/)
+// })
